e5ebf31e258fde7ab14576cf8f3be67a42377856
chyzwar <schizek.marcin@gmail.com>
2025-05-16 20:58:54 +0200

    feat(runner,eslint-config): added logFile option in runner, eslint update deps

--- packages/runner/src/spawnTask.ts
+++ packages/runner/src/spawnTask.ts
@@ -1,9 +1,21 @@
-import type { SpawnOptions } from "child_process";
+import { createWriteStream, existsSync, rmSync } from "fs";
 import { spawn } from "child_process";
-
 import register from "./register.js";
 import Logger from "./Logger.js";
 import SpawnError from "./SpawnError.js";
+import type { SpawnOptions } from "child_process";
+
+interface SpawnTaskOptions extends SpawnOptions {
+  /**
+   * Debug mode, print full docker command
+   */
+  debug?: boolean;
+
+  /**
+   * Log stdout and stderr to file
+   */
+  logFile?: string;
+}
 
 /**
  * Create task to spawn process
@@ -12,7 +24,7 @@ import SpawnError from "./SpawnError.js";
  * @param args arguments for command
  * @param options spawn option
  */
-export function spawnTask(taskName: string, command: string, args: string[] = [], options: SpawnOptions = {}): void {
+export function spawnTask(taskName: string, command: string, args: string[] = [], options: SpawnTaskOptions = {}): void {
   async function spawnTaskFunction(): Promise<void> {
     const logger = new Logger(taskName);
     logger.info("Started task");
@@ -29,47 +41,61 @@ export function spawnTask(taskName: string, command: string, args: string[] = []
 
     const proc = spawn(command, args, spawnOptions);
 
+    if (options.debug) {
+      logger.info(`${command} ${args.join(" ")}`);
+    }
+
     return new Promise<void>((resolve, reject) => {
-      proc.stdout?.on("data", (data?: Buffer) => {
-        if (data) {
-          data
-            .toString()
-            .split(/\r?\n/)
-            .filter(s => s !== "")
-            .forEach((line: string) => {
-              logger.info(line);
-            });
+      if (options.logFile) {
+        if (existsSync(options.logFile)) {
+          rmSync(options.logFile);
         }
-      });
+        const logFile = createWriteStream(options.logFile, { flags: "a" });
+        proc.stdout?.pipe(logFile);
+        proc.stderr?.pipe(logFile);
+      }
+      else {
+        proc.stdout?.on("data", (data?: Buffer) => {
+          if (data) {
+            data
+              .toString()
+              .split(/\r?\n/)
+              .filter(s => s !== "")
+              .forEach((line: string) => {
+                logger.info(line);
+              });
+          }
+        });
 
-      proc.stderr?.on("data", (data?: Buffer) => {
-        if (data) {
-          data
-            .toString()
-            .split(/\r?\n/)
-            .filter(s => s !== "")
-            .forEach((line: string) => {
-              logger.info(line);
-            });
-        }
-      });
+        proc.stderr?.on("data", (data?: Buffer) => {
+          if (data) {
+            data
+              .toString()
+              .split(/\r?\n/)
+              .filter(s => s !== "")
+              .forEach((line: string) => {
+                logger.info(line);
+              });
+          }
+        });
 
-      proc.on("error", (error) => {
-        logger.error(`Task <${taskName}> failed with:`, error);
-      });
+        proc.on("error", (error) => {
+          logger.error(`Task <${taskName}> failed with:`, error);
+        });
 
-      proc.on("close", (code: number) => {
-        if (code === 0) {
-          logger.timeEnd("Task completed in");
-          resolve();
-        }
-        else {
-          logger.error(`Failed with code: ${code}`);
-          reject(
-            new SpawnError("Spawn Task closed with non-zero exit code", code, taskName),
-          );
-        }
-      });
+        proc.on("close", (code: number) => {
+          if (code === 0) {
+            logger.timeEnd("Task completed in");
+            resolve();
+          }
+          else {
+            logger.error(`Failed with code: ${code}`);
+            reject(
+              new SpawnError("Spawn Task closed with non-zero exit code", code, taskName),
+            );
+          }
+        });
+      }
     });
   }
 
